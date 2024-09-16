a2ba4c5ef36919c647223d7aa0e3e547c93127b5
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2024-09-16 14:36:26 +0200

    4.3 Added a Logger

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use rust_mail::configuration::get_configuration;
 use rust_mail::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection = PgPool::connect(&configuration.database.connection_string())
         .await
