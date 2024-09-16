c20b3b8d16ada8befcd5d3bcacab7312a4de0a6a
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2024-09-16 13:26:57 +0200

    Cleaning sqlx before loading to github actions

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -21,7 +21,6 @@ APP_DB_NAME="${APP_DB_NAME:=newsletter}"
 # Allow to skip Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]
 then
-  # if a postgres container is running, print instructions to kill it and exit
   RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
   if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
     echo >&2 "there is a postgres container already running, kill it with"
